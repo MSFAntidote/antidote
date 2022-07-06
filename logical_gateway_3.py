5040f9b82ab988b3d7e53cfa7563ef74c70eaac2
devonmeier <108502823+devonmeier@users.noreply.github.com>
2022-07-05 18:37:49 -0600

    Update logical_gateway_3.py

--- logical_gateway_3.py
+++ logical_gateway_3.py
@@ -8,7 +8,7 @@ def display_menu(options, footer):
   border = "-" * 137
   line = ""
   
-  print(border + "\nmsfAntidote v1.0 2022 Aaron Picard and Devon Meier\n" + border)
+  print(border + "\nmsfAntidote v1.0\n" + border)
   
   for element in range(1, num_elements + 1):
     line += "[" + str(element).rjust(2) + "] " + options[element - 1].ljust(41)
@@ -21,67 +21,88 @@ def display_menu(options, footer):
 
 def add_code_submenu():
   print("\nadd_code submenu")
-
+  input("Press any key to continue...\n")
+  
 def architecture_submenu():
   print("\narchitecture submenu")
+  input("Press any key to continue...\n")
 
 def bad_chars_submenu():
   print("\nbad-chars submenu")
-
+  input("Press any key to continue...\n")
+  
 def encoder_submenu():
   print("\nencoder submenu")
-
+  input("Press any key to continue...\n")
+  
 def encoder_space_submenu():
   print("\nencoder-space submenu")
-
+  input("Press any key to continue...\n")
+  
 def encrypt_submenu():
   print("\nencrypt submenu")
-
+  input("Press any key to continue...\n")
+  
 def encrypt_iv_submenu():
   print("\nencrypt-iv submenu")
-
+  input("Press any key to continue...\n")
+  
 def encrypt_key_submenu():
   print("\nencrypt-key submenu")
-
+  input("Press any key to continue...\n")
+  
 def iterations_submenu():
   print("\niterations submenu")
-
+  input("Press any key to continue...\n")
+  
 def keep_submenu():
   print("\nkeep submenu")
-
+  input("Press any key to continue...\n")
+  
 def nopsled_submenu():
   print("\nnopsled submenu")
-
+  input("Press any key to continue...\n")
+  
 def out_submenu():
   print("\nout submenu")
-
+  input("Press any key to continue...\n")
+  
 def payload_submenu():
   print("\npayload submenu")
-
+  input("Press any key to continue...\n")
+  
 def platform_submenu():
   print("\nplatform submenu")
-
+  input("Press any key to continue...\n")
+  
 def sec_name_submenu():
   print("\nsec-name submenu")
-
+  input("Press any key to continue...\n")
+  
 def service_name_submenu():
   print("\nservice-name submenu")
-
+  input("Press any key to continue...\n")
+  
 def smallest_submenu():
   print("\nsmallest submenu")
-
+  input("Press any key to continue...\n")
+  
 def space_submenu():
   print("\nspace submenu")
-
+  input("Press any key to continue...\n")
+  
 def template_submenu():
   print("\ntemplate submenu")
-
+  input("Press any key to continue...\n")
+  
 def timeout_submenu():
   print("\ntimeout submenu")
-
+  input("Press any key to continue...\n")
+  
 def var_name_submenu():
   print("\nvar-name submenu")
-
+  input("Press any key to continue...\n")
+  
 def main():
   print("splash page code\n")
   selection = "0"
