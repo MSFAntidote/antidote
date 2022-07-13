8ddef508a2762f4dedfea08a7125c8aa28f14e11
devonmeier <108502823+devonmeier@users.noreply.github.com>
2022-07-12 19:58:45 -0600

    Update FUNCTION lport.py

--- FUNCTION lport.py
+++ FUNCTION lport.py
@@ -1,5 +1,5 @@
 def lport_submenu():
-  bad_port = "\nInvalid port number. Please enter an integer between 0 and 65535."
+  bad_lport = "\nInvalid port number. Please enter an integer between 0 and 65535."
   loop = True
 
   while loop:
@@ -8,12 +8,12 @@ def lport_submenu():
       globals()["lport_value"] = int(input("\nPlease enter a port number: "))
 
     except ValueError:
-      print(bad_port)
+      print(bad_lport)
     else:
       if lport_value in range(65536):
         print("\nLocal port set.")
         loop = False
       else:
-        print(bad_port)
+        print(bad_lport)
 
   globals()["lport_value"] = str(lport_value)
