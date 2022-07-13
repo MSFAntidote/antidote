8586882b00e2d3f72851bfda7b17a55223e5a90b
devonmeier <108502823+devonmeier@users.noreply.github.com>
2022-07-12 19:58:09 -0600

    Update FUNCTION nopsled.py

--- FUNCTION nopsled.py
+++ FUNCTION nopsled.py
@@ -1,9 +1,14 @@
 def nopsled_submenu():
+  loop = True
 
-  try:
-    nopsled = int(input("\nEnter an integer for the nopsled value: "))
-  except:
-    print("Invalid nopsled value.")
-  else:
-    globals()["nopsled_value"] = str(nopsled)
-    print("Nopsled value set.")
+  while loop:
+
+    try:
+      globals()["nopsled_value"] = int(input("\nPlease enter a nopsled value: "))
+    except ValueError:
+      print("\nInvalid nopsled value. Please enter an integer.")
+    else:
+      print("Nopsled value set.")
+      loop = False
+
+  globals()["nopsled_value"] = str(nopsled_value)
