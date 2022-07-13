f9c9858a632d49bf8d838c89290d298b7623e2d7
aaronpicard <103234130+aaronpicard@users.noreply.github.com>
2022-07-12 22:28:24 -0400

    file names change

--- lhost.py
+++
@@ -1,7 +0,0 @@
-def lhost_submenu():
-    lhost = input("Please enter local IP address: ")
-
-    try: 
-        globals()["lhost_value"] = str(ipaddress.ip_address(lhost))
-    except:
-        print("Invalid IP address")
--- rhost.py
+++
@@ -1,7 +0,0 @@
-def rhost_submenu():
-    rhost = input("Please enter Target IP address: ")
-
-    try: 
-        globals()["rhost_value"] = str(ipaddress.ip_address(rhost))
-    except:
-        print("Invalid IP address")
