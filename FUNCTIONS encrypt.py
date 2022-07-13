252088e4ea871edc658bf295d0f45aa3b192a04e
devonmeier <108502823+devonmeier@users.noreply.github.com>
2022-07-13 10:07:51 -0600

    Update FUNCTIONS encrypt.py

--- FUNCTIONS encrypt.py
+++ FUNCTIONS encrypt.py
@@ -6,7 +6,7 @@ def encrypt_submenu():
     print('\nImporting encryptions from msfvenom framework.  Please wait...')
     probe = [encr.lstrip().split(" ")[0] for encr in subprocess.getoutput("msfvenom --list encrypt").split("\n")[6:-1]]
     globals()["encrypt_options"] = {str(item + 1): probe[item] for item in range(0, len(probe))}
-    print("Complete")
+    print("Complete.")
         
   try:
     globals()["encrypt_value"] = encrypt_options[submenu_input(encrypt_options)]
