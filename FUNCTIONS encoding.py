151030465b6f57ddfbaf85e298c7fcf7c2e08080
aaronpicard <103234130+aaronpicard@users.noreply.github.com>
2022-07-12 23:29:50 -0400

    Update FUNCTIONS encoding.py

--- FUNCTIONS encoding.py
+++ FUNCTIONS encoding.py
@@ -1,12 +1,12 @@
 def encoding_submenu():
 
     try:
-        globals()["encoding_options"] = encoding_options
+        globals()["\nencoding_options"] = encoding_options
     except NameError:
 
         print('Importing encoders from msfvenom framework.  Please wait...')
         probe = [enco.lstrip().split(" ")[0] for enco in subprocess.getoutput("msfvenom --list encoders").split("\n")[6:-1]]
-        globals()["architectures_options"] = {str(item + 1): probe[item] for item in range(0, len(probe))}
+        globals()["encoding_options"] = {str(item + 1): probe[item] for item in range(0, len(probe))}
         print("Complete")
         
     try:
