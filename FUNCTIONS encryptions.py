a30486098574ed2206a35759c156bc8f30328dba
aaronpicard <103234130+aaronpicard@users.noreply.github.com>
2022-07-12 23:32:02 -0400

    Update FUNCTIONS encryptions.py

--- FUNCTIONS encryptions.py
+++ FUNCTIONS encryptions.py
@@ -1,9 +1,9 @@
 def encryptions_submenu():
 
     try:
-        globals()["encryptions_options"] = architectures_options
+        globals()["encryptions_options"] = encryptions_options
     except NameError:
-        print('Importing encryptions from msfvenom framework.  Please wait...')
+        print('\nImporting encryptions from msfvenom framework.  Please wait...')
         probe = [encr.lstrip().split(" ")[0] for encr in subprocess.getoutput("msfvenom --list encrypt").split("\n")[6:-1]]
         globals()["encryptions_options"] = {str(item + 1): probe[item] for item in range(0, len(probe))}
         print("Complete")
