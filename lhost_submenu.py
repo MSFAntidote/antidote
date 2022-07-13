20b2cc8eaa9859b3ff38ad11f50d3d878e3c74ac
aaronpicard <103234130+aaronpicard@users.noreply.github.com>
2022-07-12 22:27:30 -0400

    lhost, rhost submenus

---
+++ lhost_submenu.py
@@ -0,0 +1,11 @@
+def lhost_submenu():
+  globals()["lhost_value"] = ""
+  while lhost_value == "":
+    try:
+      lhost = input("\nPlease enter a local IP address: ")
+      lhost_test = ipaddress.ip_address(lhost)
+    except ValueError:
+      print("\nInvalid IP address.")
+    else:
+      print("Local host set.")
+      globals()["lhost_value"] = lhost
